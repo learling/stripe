8ea98c93d6f88dae9276e3b233c88dd05b21b7ab
ivannit <dev@ivanne.de>
2021-08-04 13:40:30 +0200

    remove duplicate href

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,4 @@
 <footer class="footer bg-dark p-2 text-center mt-5">
-  <a href="/impressum.php" class="text-white mr-4" href="#">Impressum</a>
-  <a href="/datenschutz.php" class="text-white" href="#">Datenschutz</a>
+  <a href="/impressum.php" class="text-white mr-4">Impressum</a>
+  <a href="/datenschutz.php" class="text-white">Datenschutz</a>
 </footer>
